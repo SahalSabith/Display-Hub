793eb1f3473e0e271ceea0128ff2252f9436b2b2
Sahal Sabith <sahalsabith111@gmail.com>
2024-10-20 17:45:39 +0530

    DisplayHub 2.0.2

--- displayHub/static/js/sw.js
+++ displayHub/static/js/sw.js
@@ -2,9 +2,18 @@ self.addEventListener('push', function(event) {
     const data = event.data.json();
     const options = {
         body: data.body,
-        badge: data.badge
+        data: {
+            url: data.url
+        }
     };
     event.waitUntil(
         self.registration.showNotification(data.head, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
     );
 });
