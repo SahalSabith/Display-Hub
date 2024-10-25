a6cda64a512598e775027ef64ec5b7fc02927aa5
Sahal Sabith <sahalsabith111@gmail.com>
2024-10-25 20:26:44 +0530

    DisplayHub 4.0.9

--- displayHub/talk/views.py
+++ displayHub/talk/views.py
@@ -43,7 +43,7 @@ def userMessage(request, chatroomName=None):
 @login_required(login_url='signIn')
 def getOrCreateChatroom(request):
     # Get the admin user (consider using a more dynamic approach)
-    admin = User.objects.get(pk=12)
+    admin = User.objects.get(pk=1)
     # Get all private chatrooms for the current user
     chatrooms = request.user.userChat.filter(isPrivate=True)
 
